7c4837faf6e0e389fb98db1d2f58bb95e9dde59c
Alice Groudko <alice.groudko@embl.de>
2022-08-23 10:32:52 +0200

    del file

--- jobs.sh
+++
@@ -1,6 +0,0 @@
-#bash /g/scb2/bork/groudko/scripts/ref_v1.1/pipe.sh -s 'Fusobacterium nucleatum' -d /g/scb/bork/groudko/ref_v1.1 2>>pipe.log &
-#bash /g/scb2/bork/groudko/scripts/ref_v1.1/pipe.sh -s 'Fusobacterium nucleatum_J' -d /g/scb/bork/groudko/ref_v1.1 2>>pipe.log &
-bash /g/scb2/bork/groudko/scripts/ref_v1.1/pipe.sh -s 'ANI_95_v1_000547972' -d /g/scb/bork/groudko/ref_v1.1 2>>pipe.log &
-bash /g/scb2/bork/groudko/scripts/ref_v1.1/pipe.sh -s 'Clostridioides difficile' -d /g/scb/bork/groudko/ref_v1.1 2>>pipe.log &
-#bash /g/scb2/bork/groudko/scripts/ref_v1.1/pipe.sh -s 'ANI_95_v1_000537534' -d /g/scb/bork/groudko/ref_v1.1 2>>pipe.log &
-#disown -a
